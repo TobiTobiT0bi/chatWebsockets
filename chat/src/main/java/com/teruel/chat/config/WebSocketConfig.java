50d45cd89f376f68d826a9a105ec0622928e9520
TobiTobiT0bi <Tobias.coman@hotmail.com>
2024-10-14 21:41:44 -0300

    completo en su totalidad el backend del chat en springboot con websockets

---
+++ chat/src/main/java/com/teruel/chat/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.teruel.chat.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.teruel.chat.services.ChatHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Autowired
+	private ChatHandler chatHandler;
+	
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addHandler(chatHandler, "/chat");
+		
+	}
+	
+}
